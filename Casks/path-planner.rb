71cbc32e25b93890ea4a06c6ae8ae6b243ce4b4f
Cabnit-rs <jkistler28@windhamsd.org>
2025-09-24 17:13:36 -0400

    Update path-planner.rb

--- Casks/path-planner.rb
+++ Casks/path-planner.rb
@@ -2,8 +2,8 @@ cask "path-planner" do
   version "2025.2.2"
   sha256 ""
 
-  url "https://github.com/mjansen4857/pathplanner/releases/tag/v2025.2.2/v#{version}/PathPlanner-macOS-v#{version}.dmg",
-     verified: "github.com/mjansen4857/pathplanner/"
+  url "https://github.com/Cabnit-rs/pathplanner/releases/tag/yep/PathPlanner-macOS-v#{version}.dmg",
+     verified: "https://github.com/Cabnit-rs/pathplanner"
   name "PathPlanner"
   desc "Tool to plan paths"
   homepage "https://pathplanner.dev/home.html"
