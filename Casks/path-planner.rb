06556c327779b51d080e58156085689a854a5640
Cabnit-rs <jkistler28@windhamsd.org>
2025-09-24 12:42:37 -0400

    Update path-planner.rb

--- Casks/path-planner.rb
+++ Casks/path-planner.rb
@@ -1,6 +1,7 @@
 cask "path-planner" do
   version "2025.2.2"
-  sha256 ""
+  sha256 Expected: "68684ebd2a18ea115f63167b2fc5ed22ef3efb00a025cbac44bc1c54e197369d",
+  Actual: "f882be73a7d5f7aca6ef95f630b42c41215c87deadcc19fb1b685bfa1195b086"
 
   url "https://github.com/mjansen4857/pathplanner/releases/tag/v2025.2.2/v#{version}/PathPlanner-macOS-v#{version}.dmg"
      
